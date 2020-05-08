e00ebdea547b6948d08edf2d19d910399d1108ed
NderituMwanu <mwanugerald@gmail.com>
2020-05-08 19:13:21 +0300

    contains email and password for flask mail opertaion

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-export MAIL_USERNAME= 'jisnderi@gmail.com'
-export MAIL_PASSWORD= 'Kenyatta100@#'
+export MAIL_USERNAME= <enter your email>
+export MAIL_PASSWORD= <enter your password>
 python3.8 manage.py server
